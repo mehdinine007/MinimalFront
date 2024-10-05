020c55a494860766d6254368b7420c0c05ad19ae
Nohtorki <Nohtorki@NOHTORKI-PC>
2024-10-05 14:58:40 +0330

    fix: ckeditor table

--- public/appsettings.js
+++ public/appsettings.js
@@ -1,25 +1,17 @@
-
 //   const order = "https://sorder.iranfab.com/api";
 // const user = "https://suser.iranfab.com/api";
 // const captcha = "https://scaptcha.aria-vision.com/Home/GetNumericCaptchaImage";
 //  const payment = "https://spayment.iranfab.com/api";
 
-
-
- const order = "https://mmorder.iranfab.com/api";
- const user = "https://mmuser.iranfab.com/api";
- const captcha = "https://mmcaptcha.aria-vision.com/Home/GetNumericCaptchaImage";
-  const payment = "https://mmpayment.aria-vision.com/api";
-  const siteTitle= "Max Motor";
+const order = "https://sorder.iranfab.com/api";
+const user = "https://suser.iranfab.com/api";
+const captcha = "https://scaptcha.aria-vision.com/Home/GetNumericCaptchaImage";
+const payment = "https://spayment.aria-vision.com/api";
+const siteTitle = "Max Motor";
 
 //const order = "http://localhost:44215/api";
 //const user = "http://localhost:44213/api";
 //const captcha = "https://mcaptcha.aria-vision.com/Home/GetNumericCaptchaImage";
- //const payment = "https://localhost:44345/api";
- //const admin = "https://localhost:44340/api";
-  //const siteTitle= "Max Motor";
-
-
-
-
-
+//const payment = "https://localhost:44345/api";
+//const admin = "https://localhost:44340/api";
+//const siteTitle= "Max Motor";
